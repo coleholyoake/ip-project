ff5bc5e9a0d2dc2e86c4bd321114a0822870bb5e
Cole Holyoake <coleholyoake@live.com>
2016-03-11 13:12:55 +1300

    image animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,9 @@
     <body>
         <a-scene>
                 
-
+                <a-assets>
+                    <a-asset-item id="test" src="model/Hand.dae"></a-asset-item>
+                </a-assets>
 
 
 
@@ -48,7 +50,12 @@
 
                  {{-- <a-curvedimage src="img/Untitled.png" radius="5.7" theta-length="50" height="3" rotation="0 25 0" position="0 4 0" opacity="0.8"></a-curvedimage> --}}
 
-                 <a-image src="img/Untitled.png" height=".8" opacity="0.8" position="0 2.35 0" scale="0.8"></a-image>
+                 <a-image src="img/Untitledcopy.png" height="4" opacity="0.8" position="0 3 -3" scale="4">
+                    <a-animation attribute="scale" from="1 0 1" to="3 0.75 1" begin="1500" dur="200" fill="both" easing="ease-out"></a-animation>
+                    <a-animation attribute="position" from="0 5 0" to="0 3 -2" begin="1000" dur="1000" fill="both" easing="ease-out"></a-animation>
+                 </a-image>
+
+
 
                 {{-- <a-entity geometry=     "primitive: plane;
                                         width: .6;
@@ -85,28 +92,28 @@
                 </a-entity> --}}
 
                 <a-entity geometry=     "primitive: plane;
-                                        width: .6;
-                                        height: .2;
+                                        width: 1.8;
+                                        height: .5;
                                         translate: 0 0 0;"
                             material=   "color: #d3d3d3;
                                         opacity: 0.8;"
-                            position=   "0 1.5 0">
+                            position=   "0 1.2 -3">
                 <a-mouseenter material= "color: #fff;
                                         opacity: 0.8;"
-                            position=   "0 1.5 0.08">
+                            position=   "0 1.2 -2.8">
                 </a-mouseenter>
                 <a-mouseleave material= "color: #c3c3c3;
                                         opacity: 0.8;"
-                            position=   "0 1.5 0">
+                            position=   "0 1.2 -3">
                 </a-mouseleave>
                 </a-entity>
 
                 <a-sky src="img/sky4.jpg"></a-sky>
 
-                <a-model scale=".5 .5 .5" src="model/Hand.dae" position="1.2 1 -1" rotation="0 20 0"></a-model>
+                <a-model scale=".5 .5 .5" src="model/Hand.dae" position="1.2 1 -2" rotation="0 20 0"></a-model>
                  
             
-                <a-camera position="0 1.8 1.5"></a-camera>
+                <a-camera position="0 1.8 1.5" wasd-controls-enabled="false"></a-camera>
                 <a-light color="#fff" position="0 1.8 1.5" intensity="2"></a-light>
         </a-scene>
         <img src="img/fallback_sky.jpg">
