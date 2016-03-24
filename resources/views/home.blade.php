9b7e30b87d2f070a9ca62863e80453315382d761
Cole Holyoake <coleholyoake@live.com>
2016-03-25 09:25:54 +1300

    cleaned up indentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
 <div class="above-the-fold">
-    <!-- <h1>Some Above-The-Fold Content</h1> -->
     <div class="container fold">	
     	<div class="row hero-logo">
     		<div class="col-md-4 col-md-offset-4">
@@ -11,110 +10,48 @@
     		</div>
     	</div>
     	<div class="row">
-	    	
-	    		<img id="before" class="visible center-block scroll-down" src="img/scroll_arrow.svg" width="6%">
-	    		<img id="after" class="hidden center-block scroll-down" src="img/scroll_text.svg" width="6%">
-	    	
+	    	<img id="before" class="visible center-block scroll-down" src="img/scroll_arrow.svg" width="6%">
+	    	<img id="after" class="hidden center-block scroll-down" src="img/scroll_text.svg" width="6%">
 	    </div>
-    	
-    	
-	    
 	</div>
 </div>
 
-<!-- <div class="">
-    		<div class="table-cell">
-    			
-	    		<img src="img/front.png" width="50%" class="center-block">
-	    		
-    		</div>
-    		<div class="table-cell2">
-    			
-    			
-	    			<p>fg Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	    			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	    			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	    			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	    			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	    			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    			
-    		</div>
-    	</div> -->
+
 <div class="containr nopadmarg">
-    		<div class="row nopadmarg">
-
-    		
-    		<div class="col-md-8 nopadmarg table-cell">
-    			
-    			<div class="row nopadmarg deli" >
+    <div class="row nopadmarg">
+		<div class="col-md-8 nopadmarg table-cell">
+    		<div class="row nopadmarg deli" >
 	    			
-    			</div>
     		</div>
-    			<div class="col-md-4 cell5 nopadmarg table-cell2">
-    				<div class="row nopadmarg deli" >
-    				<h2>INTRO</h2>
-    				
-    			
-	    			
-    			
-    		</div>
-    		
-    		</div>
-    		
     	</div>
-
-<!-- <div class="clear">
-	<div class="containr nopadmarg">
-    		<div class="row nopadmarg">
-    			<div class="col-md-12 cell4 nopadmarg">
-    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    			<p>fg Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	    			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	    			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	    			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	    			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	    			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	    			<p>fg Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	    			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	    			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	    			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	    			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	    			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    			</div>
+    	<div class="col-md-4 cell5 nopadmarg table-cell2">
+    		<div class="row nopadmarg deli" >
+    			<h2>INTRO</h2>
     		</div>
+    	</div>
     </div>
-</div> -->
+</div>
+
 <div class="clear">
 	<div class="containr nopadmarg">
-    		<div class="row nopadmarg">
-
-    		
-    		<div class="col-md-6 cell4 nopadmarg captdeli">
-    			
-    			<div class="row nopadmarg deli" >
+    	<div class="row nopadmarg">
+			<div class="col-md-6 cell4 nopadmarg captdeli">
+	    		<div class="row nopadmarg deli" >
 	    			<h2>LIVE</h2>
     			</div>
     		</div>
-    			<div class="col-md-6 cell4 nopadmarg ">
-    				<div class="row nopadmarg capt" >
+    		<div class="col-md-6 cell4 nopadmarg ">
+    			<div class="row nopadmarg capt" >
     				<h2>CAPTURE</h2>
-    				</div><div class="row nopadmarg capt">
+    			</div>
+    			<div class="row nopadmarg capt">
     				<h2>DELIVERY</h2>
-    				</div>
-    			
-	    			
-    			
+    			</div>
     		</div>
-    		
-    		</div>
-    		
     	</div>
+    </div>
 </div>
+
 <div class="clear">
 	<div class="containr nopadmarg">
     	<div class="row nopadmarg">
@@ -123,30 +60,20 @@
 	    			<h2>PROJECTS</h2>
     			</div>
     		</div>
-    		<!-- <div class="col-md-4 cell3 nopadmarg">
-    			
+    		<div class="col-md-7 cell5 nopadmarg ">
     			<div class="row nopadmarg deli" >
-	    			<h2>LIVE</h2>
-    			</div>
-    		</div> -->
-    			<div class="col-md-7 cell5 nopadmarg ">
-    				<div class="row nopadmarg deli" >
     				<h2>INFO</h2>
-    				</div>
-	    			
-    			
-    		</div>
+    			</div>
+	    	</div>
     		<div class="col-md-1 cell5 nopadmarg ">
-    				<div class="row nopadmarg soci" ></div>
-    				<div class="row nopadmarg soci" ></div>
-	    			<div class="row nopadmarg soci" ></div>
-    				<div class="row nopadmarg soci" ></div>
-    				<div class="row nopadmarg soci" ></div>
+    			<div class="row nopadmarg soci" ></div>
+    			<div class="row nopadmarg soci" ></div>
+	    		<div class="row nopadmarg soci" ></div>
+    			<div class="row nopadmarg soci" ></div>
+    			<div class="row nopadmarg soci" ></div>
     		</div>
-    		
-    		</div>
-    		
     	</div>
+    </div>
 </div>
 
 @endsection
